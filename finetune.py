8b4cc674910ccc3498abc52fbe8d3d6d0d0266cf
smdesai27 <sanilmdesai@gmail.com>
2025-08-29 17:59:33 -0500

    added finetume

---
+++ finetune.py
@@ -0,0 +1,5 @@
+import sys, torch, sklearn, transformers
+print(sys.executable)                 # should show ...\envs\nlp-project\python.exe
+print("torch:", torch.__version__)
+print("sklearn:", sklearn.__version__)
+print("transformers:", transformers.__version__)
